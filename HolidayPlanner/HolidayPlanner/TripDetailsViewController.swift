505498194e3569a7e91fdc27443b206f5af32508
Hdsenevi <shanaka36@gmail.com>
2020-02-21 06:53:10 +1100

    Adding controls

--- HolidayPlanner/HolidayPlanner/TripDetailsViewController.swift
+++ HolidayPlanner/HolidayPlanner/TripDetailsViewController.swift
@@ -10,9 +10,38 @@ import Cocoa
 
 class TripDetailsViewController: NSViewController {
 
+    @IBOutlet weak var startDatePicker: NSDatePicker!
+    @IBOutlet weak var endDatePicker: NSDatePicker!
+    @IBOutlet weak var totalTimeLabel: NSTextField!
+    
+    lazy var deteComponentsFormatter: DateComponentsFormatter = {
+        let dateComponentsFormatter = DateComponentsFormatter()
+        
+        dateComponentsFormatter.allowedUnits = .day
+        dateComponentsFormatter.unitsStyle = .full
+        
+        return dateComponentsFormatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        self.startDatePicker.dateValue = Date()
+        self.endDatePicker.dateValue = Date()
+        
+        dateChanged(self.startDatePicker)
     }
     
+    
+    @IBAction func dateChanged(_ sender: NSDatePicker) {
+        let cal = Calendar(identifier: .gregorian)
+        let midnightStartDate = cal.startOfDay(for: self.startDatePicker.dateValue)
+        
+        // To make even days we go through midnight of the next day
+        let midnightEndDate = cal.startOfDay(for: self.endDatePicker.dateValue).addingTimeInterval(60 * 60 * 24)
+        
+        let tripTime = midnightEndDate.timeIntervalSince(midnightStartDate)
+        
+        self.totalTimeLabel.stringValue = self.deteComponentsFormatter.string(from: tripTime)!
+    }
 }
